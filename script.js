8d128205b518d4f5c521141f05a994d9cbdd587e
= <aminicomputer96@gmail.com>
2021-05-31 14:48:26 +0430

    js ms=sg

---
+++ script.js
@@ -0,0 +1,24 @@
+const currencyEl_one = document.getElementById('currency-one');
+const amountEl_one = document.getElementById('amount-one');
+const currencyEl_two = document.getElementById('currency-two');
+const amountEl_two = document.getElementById('amount-two');
+const rateEl = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+
+// Fetch exchange rate and update the dom 
+
+
+function calculate(){
+console.log('ran')
+
+}
+
+    //event listiners
+    currencyEl_one.addEventListener('change',calculate);
+    amountEl_one.addEventListener('input',calculate);
+    currencyEl_two.addEventListener('change',calculate);
+    currencyEl_two.addEventListener('input',calculate);
+
+
+calculate();
